c54c03f35551a82ed4516b3efaaf8b81c5ae9c88
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-06-20 18:53:54 -0300

    Create inverte-valores-digitados.py
    Atualização 20/06/2023 18:53

---
+++ Módulo-1/Aula-2/inverte-valores-digitados.py
@@ -0,0 +1,10 @@
+# Inverter os valores digitados
+
+A = int(input('Entre com o valor de A: '))
+B = int(input('Entre com o valor de B: '))
+
+C = A
+A = B
+B = C
+
+print(f'O valor de A = {A} e o valor de B = {B}.')
