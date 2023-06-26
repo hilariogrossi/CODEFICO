178a027f0da22f029ca6e658665a130cedab7308
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-06-26 18:51:51 -0300

    Create reais-inteiros-raiz-cosseno-arredondamento.-cima-baixo.py
    Atualização 26/06/2023 18:51

---
+++ Módulo-1/Aula-6/reais-inteiros-raiz-cosseno-arredondamento.-cima-baixo.py
@@ -0,0 +1,21 @@
+# Faça um programa que solicita ao usuário informar inicialmente se deseja realizar cálculos com valores "reais" ou "inteiros".
+# O usuário deve entrar com "real" ou "inteiro" e caso o texto digitado não seja um destes dois o programa deve ser encerrado. Caso contrário o programa
+# solicita ao usuário que entre com um valor do tipo escolhido. No caso de ter escolhido trabalhar com número inteiro o programa deve informar a
+# raiz quadrada do número e também o seu cosseno. No caso de ter escolhido trabalhar com número real o programa deve usar as funções de arredondamente
+# para imprimir os valores arrendondados para cima e para baixo.
+
+import math
+
+tipo = input('Digite inteiro ou real para prosseguir: ').lower()
+
+if tipo != 'inteiro' and tipo != 'real':
+    print('VOCÊ NÃO DIGITOU CORRETAMENTE. Encerrando o programa...')
+else:
+    if tipo == 'inteiro':
+        num = int(input('Entre com número para processamento: '))
+        raiz_quadrada = math.sqrt(num)
+        cosseno = math.cos(num)
+        print(f'A raiz quadrada do número {num} é: {raiz_quadrada}\nO cosseno do número {num} é: {cosseno}.')
+    else:
+        num = float(input('Entre com o número para processamento: '))
+        print(f'O arrendondamento para baixo do número {num} é: {math.floor(num)}\nO arredondamento para cima do número {num} é: {math.ceil(num)}')
