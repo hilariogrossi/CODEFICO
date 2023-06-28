12773af46fb30def7a4be9ab1ff3600c99d23acf
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-06-28 08:46:39 -0300

    Create maior-meio-menor-0-ate-maior-pares.py
    Atualização 28/06/2023 08:46

---
+++ Módulo-1/Aula-7/maior-meio-menor-0-ate-maior-pares.py
@@ -0,0 +1,48 @@
+'''Codificar um programa que leia 3 valores numéricos positivos pelo teclado. O programa deve inicialmente checar se todos os valores são positivos e caso algum não
+seja deve solicitar nova entrada de dados. O programa só poderá passar adiante quando os 3 valores informados forem positivos. O programa deve criar 3 variáveis:
+maior valor, o segundo maior valor e o menor valor dentre os 3 valores números positivos da entrada. Ao final imprimir os valores das variáveis menor, meio e maior
+apresentando-os em ordem crescente.
+Modifique o programa do exercício 5 para fazer também a seguinte tarefa: imprimir todos os números PARES entre 0 e o maior valor encontrado.'''
+
+n1 = float(input('\nEntre com primeiro número positivo: '))
+n2 = float(input('\nEntre com segundo número positivo: '))
+n3 = float(input('\nEntre com terceiro número positivo: '))
+
+while n1 < 0 or n2 < 0 or n3 < 0:
+    print('\nVOCÊ ENTROU COM UM NÚMERO NEGATIVO. Repita a operação!')
+    n1 = float(input('\nEntre com primeiro número positivo: '))
+    n2 = float(input('\nEntre com segundo número positivo: '))
+    n3 = float(input('\nEntre com terceiro número positivo: '))
+
+if n1 > n2 > n3:
+    maior = n1
+    if n2 > n3:
+        meio = n2
+        menor = n3
+    else:
+        meio = n3
+        menor = n2
+elif n2 > n1 > n3:
+    maior = n2
+    if n1 > n3:
+        meio = n1
+        menor = n3
+    else:
+        meio = n3
+        menor = n1
+else:
+    maior = n3
+    if n1 > n2:
+        meio = n1
+        menor = n2
+    else:
+        meio = n2
+        menor = n1
+
+print(f'\nO menor número é o {menor}.\nO número do meio é {meio}, e\nO maior número é {maior}.\n')
+
+pares = 0
+
+while pares <= maior:
+    print(f'\n{pares}\n\t')
+    pares += 2
