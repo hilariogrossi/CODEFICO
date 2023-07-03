217a1bc59d8479ac5aadab67c3fe01b0ef00098e
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-07-03 17:49:31 -0300

    Update def-aritmetica-penderada-media-comportamento.py
    Atualização 03/07/2023 17:49

--- Módulo-1/Aula-9/def-aritmetica-penderada-media-comportamento.py
+++ Módulo-1/Aula-9/def-aritmetica-penderada-media-comportamento.py
@@ -13,3 +13,10 @@ nota_2 = float(input('\nEntre com a segunda nota do aluno: '))
 nota_3 = float(input('\nEntre com a terceira nota do aluno: '))
 
 participacao = input('\nEntre com [B] boa e [R] ruim para a participação do aluno: ').upper()
+
+resposta, metodo = calculaNota(nota_1, nota_2, nota_3, participacao)
+
+if resposta >= 6:
+    print(f'\nO aluno foi aprovado com {resposta:.2f} e utilizando a média {metodo}!\n')
+else:
+    print(f'\nO aluno foi reprovado com {resposta:.2f} e utilizando a média {metodo}!\n')
