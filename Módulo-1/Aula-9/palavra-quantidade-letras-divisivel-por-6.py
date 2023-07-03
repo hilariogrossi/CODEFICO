1112c5df1b81d10e48fb6c90a1eacdea65da9d1e
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-07-03 17:57:28 -0300

    Create palavra-quantidade-letras-divisivel-por-6.py
    Atualização 03/07/2023 17:57

---
+++ Módulo-1/Aula-9/palavra-quantidade-letras-divisivel-por-6.py
@@ -0,0 +1,24 @@
+'''Você foi contratado para desenvolver um programa que verifique se uma palavra digitada pelo
+usuário tem uma quantidade de letras que seja divisível por 6. Para isso, você precisa implementar
+duas funções: A função verificar_tamanho_palavra(palavra) que recebe uma palavra como entrada e
+verifica se a quantidade de caracteres que ela possui, é um número divisível por 6. Caso seja,
+deve exibir uma mensagem que informa se o número de caracteres é divisível por 6 ou não. A função
+programa_tamanho_palavra() que é a função principal do programa, solicita ao usuário que digite
+uma palavra e chama a função verificar_tamanho_palavra() para verificar o tamanho da palavra
+digitada. Dica: Utilize a função len() para obter o número de caracteres da palavra.'''
+
+
+def verificar_tamanho_palavra(palavra):
+    if len(palavra) % 6 == 0:
+        print(f'\nA palavra {palavra} é divisíl por 6.\n')
+    else:
+         print(f'\nA palavra {palavra} NÃO é divisíl por 6.\n')
+
+
+def programa_tamanho_palavra():
+    palavra = input('\nEntre com uma palavra para verificação: ')
+
+    verificar_tamanho_palavra(palavra)
+
+
+programa_tamanho_palavra()
