058540e39bf1968a2169ba81e7242fe94c85b364
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-07-31 18:05:59 -0300

    Create Exercício-1.py
    Atualização 31/07/2023 18:05

---
+++ Modulo-2/Aula-10/Exercício-1.py
@@ -0,0 +1,29 @@
+'''Supondo que um algoritmo A e um algoritmo B, com funções de complexidade de tempo A(n) = n^2−n+ 549 e B(n) = 49n + 49, respectivamente.
+Determine quais valores de n pertencentes ao conjunto dos números naturais para os quais A leva menos tempo para executar do que B.
+Obs: n^2 siginifica n elevado ao quadrado, ou seja n . n'''
+
+# Exercício 1 - Lista exercício 31/07/2023
+
+'''n ** 2 - n + 549 < 49 * n + 49
+n ** 2 - n - 49 * n + 549 - 49 < 0
+n ** 2 - 50 * n + 500
+
+delta = b ** 2 - 4 * a * c
+
+a = 1
+b = = 50
+c = 500
+
+delta = 50 ** 2 - 4 * 1 * 500
+delta = 2500 - 500
+delta = 500
+
+n = (50 +/- sqrt(500)) / 2 * a
+
+n1 = (50 + 22) / 2 * a
+n1 = 77 / 2
+
+n2 = (50 - 22) / 2 * a
+n2 = 28 / 2
+
+77 / 2 < n < 28 / 2'''
