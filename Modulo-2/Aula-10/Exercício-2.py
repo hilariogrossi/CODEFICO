b4a85b76e8d8b977539257358af14afd48530816
Hilário Grossi de Oliveira <103451021+hilariogrossi@users.noreply.github.com>
2023-07-31 18:07:07 -0300

    Create Exercício-2.py
    Atualização 31/07/2023 18:07

---
+++ Modulo-2/Aula-10/Exercício-2.py
@@ -0,0 +1,30 @@
+'''Qual a ordem de complexidade do programa abaixo, em relação ao tamanho n da entrada, se considerarmos o número de comparações como medida de complexidade?'''
+
+# Exercício 2 - Lista exercício 31/07/2023
+
+def f(a, tam):
+    print()
+    print("a:", a)
+    for i in range(tam):
+        print("teste")
+        if i % 2 == 0:
+            a[i] = i
+
+
+def g(a, tam):
+    print()
+    for i in range(tam):
+        if i % 2 == 0:
+            print(a[i], end=" ")
+
+
+def main():
+    quant = int(input("Informe a quantidade de itens: "))
+    a = [0] * quant  # Inicializa uma lista de tamanho 'quant' preenchida com zeros
+    f(a, quant)
+    g(a, quant)
+
+
+main()
+
+# # f(n) = 4n ou f(4) = 4 * tamanho vezes
